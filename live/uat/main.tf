5d3ceaae676b5f05e37cf22bde1eddb14ab852ce
Justino Boggio <justinoboggio@gmail.com>
2025-11-13 20:34:02 -0300

    uat environment files

---
+++ live/uat/main.tf
@@ -0,0 +1,68 @@
+locals {
+  location  = "eastus"
+  env       = "uat"
+  tenant_id = "004b1179-227e-44f2-b759-e9f05b015b7b"
+
+  common_tags = {
+    environment = local.env
+    owner       = "justino"
+    project     = "terraform-azure-lab"
+  }
+}
+
+# Main resource group for UAT
+module "rg_core" {
+  source   = "../../modules/resource-group"
+  name     = "rg-core-${local.env}"
+  location = local.location
+  tags     = local.common_tags
+}
+
+# Main virtual network for UAT
+module "vnet_core" {
+  source = "../../modules/network"
+
+  name                = "vnet-core-${local.env}"
+  location            = local.location
+  resource_group_name = module.rg_core.name
+  address_space       = ["10.20.0.0/16"]
+
+  subnets = {
+    "snet-apps" = {
+      address_prefixes = ["10.20.1.0/24"]
+    }
+    "snet-db" = {
+      address_prefixes = ["10.20.2.0/24"]
+    }
+  }
+
+  tags = local.common_tags
+}
+
+# Shared Log Analytics workspace for UAT
+module "log_analytics_core" {
+  source = "../../modules/log-analytics"
+
+  name                = "law-core-${local.env}"
+  location            = local.location
+  resource_group_name = module.rg_core.name
+
+  retention_in_days = 30
+  tags              = local.common_tags
+}
+
+# Shared Key Vault for UAT (RBAC enabled)
+module "kv_core" {
+  source = "../../modules/key-vault"
+
+  name                = "kv-core-${local.env}"
+  location            = local.location
+  resource_group_name = module.rg_core.name
+  tenant_id           = local.tenant_id
+
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+
+  tags = local.common_tags
+}
---
+++ live/uat/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.9.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  storage_use_azuread = true
+}
