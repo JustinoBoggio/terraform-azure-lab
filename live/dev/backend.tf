4fe746775b6e5b6fe3aa466bdf0afb0988b7fda8
Justino Boggio <justinoboggio@gmail.com>
2025-11-12 23:19:09 -0300

    Fix error in container name

--- live/dev/backend.tf
+++ live/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "statfstatejustino"
-    container_name       = "tfstate"
+    container_name       = "tfstate-dev"
     key                  = "infra-dev.tfstate"
 
     # Use Azure AD + OIDC for the backend
