829b22d0bd8de4fd19add3d194031071c2168e84
Justino Boggio <justinoboggio@gmail.com>
2025-11-12 23:13:35 -0300

    added changes to use oidc in provider and CI

--- live/dev/backend.tf
+++ live/dev/backend.tf
@@ -2,7 +2,13 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "statfstatejustino"
-    container_name       = "tfstate-dev"
+    container_name       = "tfstate"
     key                  = "infra-dev.tfstate"
+
+    # Use Azure AD + OIDC for the backend
+    use_azuread_auth = true
+    use_oidc         = true
+    tenant_id        = "004b1179-227e-44f2-b759-e9f05b015b7b"
+    client_id        = "54a7c81b-4ea0-4b56-aafa-6e3daa2a976d"
   }
 }
--- live/dev/providers.tf
+++ live/dev/providers.tf
@@ -1,14 +1,18 @@
 terraform {
-  required_version = ">= 1.6.0"
-
+  required_version = ">= 1.9.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.100"
+      version = "~> 3.90"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+
+  # Force OIDC in CI
+  use_oidc            = true
+  use_cli             = false
+  storage_use_azuread = true
 }
