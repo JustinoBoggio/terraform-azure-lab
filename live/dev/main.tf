c8721fa0a541aae3b212a5beca16fb2cfee4dc4a
Justino Boggio <justinoboggio@gmail.com>
2025-11-24 00:43:50 -0300

    Added simple python app to test key vault secret read from AKS

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -171,6 +171,12 @@ module "sample_app" {
   secret_provider_class_name = "spc-hello-api"
 
   host = "hello-dev.local"
+
+  env_vars = {
+    KEYVAULT_URL = module.kv_core.vault_uri
+    SECRET_NAME  = "hello-api-message"
+    ENVIRONMENT  = local.env
+  }
 }
 
 module "hello_api_identity" {
